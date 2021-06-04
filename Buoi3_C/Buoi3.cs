1f0ed5251aaeccd623ae182138ee0d64090dad23
doanchuxuan18 <84855872+doanchuxuan18@users.noreply.github.com>
2021-06-04 22:32:09 +0700

    Buoi4

--- Buoi3_C/Buoi3.cs
+++ Buoi3_C/Buoi3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,33 +29,113 @@ namespace Buoi3_C
         static void BaiTap()
         {
             Console.WriteLine("Nhap thong tin sinh vien:");
-            NhapDiem();
+           //string _Hoten = NhapTen();
+           //Console.WriteLine("Xin chao ban "+_Hoten);
+           double _DiemHoa = NhapDiem("Hóa");
+           double _DiemLy = NhapDiem("Lý");
+           double _DiemToan = NhapDiem("Toán");
+           Console.WriteLine("Diem mon hoa cua ban la :{0}đ ", _DiemHoa);
+           Console.WriteLine("Diem mon hoa cua ban la :{0}đ ", _DiemLy);
+           Console.WriteLine("Diem mon hoa cua ban la :{0}đ ", _DiemToan);
+           double _DTb = (_DiemToan + _DiemLy + _DiemHoa) / 3;
+           string _HocLuc = DanhGiaHocLuc(_DTb);
+           Console.WriteLine("Hoc luc cua ban la : {0}",_HocLuc);
+           Console.ReadLine();
         }
-        static void NhapDiem()
-        {
+        static string NhapTen()
+            
+       {
             Console.WriteLine("Ho va ten cua ban la:");
             string _Hoten = Console.ReadLine();
             if (_Hoten != "" && _Hoten != " ") //cách 1: kiểm tra rỗng
             {
                 if (!char.IsNumber(_Hoten, 0)) //Kiểm tra nó là số hay không
                 {
-
+                    if (_Hoten.Length > 3)//kiểm tra nhập quá ngắn
+                    {
+                        return _Hoten;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ten qua ngan!");
+                        NhapTen();
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("ban chua nhap ho ten cua ban!");
-                    NhapDiem();
+                    Console.WriteLine("Vui long khong nhap so vao ho ten!");
+                    NhapTen();
                 }
             }
             else
             {
                 Console.WriteLine("ban chua nhap ho ten cua ban!");
-                NhapDiem();
+                NhapTen();
             }
             //if (string.IsNullOrEmpty(_Hoten) && string.IsNullOrWhiteSpace(_Hoten))
             //{
 
             //}
+            return "";
+        }
+        static double NhapDiem(string _TenMonHoc)
+        {
+            double _dbl_Diem = 0.0;
+            Console.Write("Diem mon {0} cua ban la: ",_TenMonHoc);
+            string _str_Diem = Console.ReadLine();
+            if (_str_Diem != "" && _str_Diem != " ")//Kiểm tra rỗng
+            {
+                if (char.IsNumber(_str_Diem, 0))//Kiểm tra có là số hay không
+                {
+                    _dbl_Diem = double.Parse(_str_Diem);
+                    if (_dbl_Diem >= 0)//Kiểm tra số âm
+                    {
+                        if (_dbl_Diem <= 10)//Kiểm tra phạm vi điểm
+                        {
+                            return _dbl_Diem;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Diem nhap khong hop ly!");
+                            NhapDiem(_str_Diem);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ban phai nhap so duong");
+                        NhapDiem(_str_Diem);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ban phai nhap so!");
+                    NhapDiem(_TenMonHoc);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ban chua nhap diem!");
+                NhapDiem(_TenMonHoc);
+            }
+            return _dbl_Diem;
+        }
+
+        static string DanhGiaHocLuc(double _Dtb)
+        {
+            string _HocLuc = "";
+            if (_Dtb >= 0 && _Dtb < 5)
+            {
+                _HocLuc = "Trung bình";
+            }
+            else if(_Dtb>=5&&_Dtb<7)
+            {
+                _HocLuc = "Khá";
+            }
+            else if (_Dtb >=7  && _Dtb <= 10)
+            {
+                _HocLuc = "Giỏi";
+            }
+            return _HocLuc;
         }
     }
 }
---
+++ Buoi4_C/Buoi4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Buoi4_C
+{
+    class Buoi4
+    {
+        static void Main()
+        {//Function (Phương thức)
+         //Phương thức 1 chiều:
+         //Cấu trúc: Static void + [Tên phương thức](){[Lệnh cần làm]};
+         //VÍ DỤ:
+         //NhiemVu1();
+
+            //Phương thức 2 chiều(Function):
+            //Cấu trúc: Static + Kiểu dữ liệu đầu ra + [Tên phương thức](){[Lệnh cần làm]};
+            //VÍ DỤ:
+            //NhiemVu2();
+            //Phương thức 2 chiều có kèm theo(Function);
+            //Cấu trúc: Static + Kiểu dữ liệu đầu ra + [Tên phương thức]([kiểu dữ liệu] + Tên biến){[Lệnh cần làm]};
+            //VÍ DỤ:
+            string _KetQuaDauRa = NhiemVu3(5,6);
+
+            Console.WriteLine("Tra ra ket qua tong 4+5 = {0}", _KetQuaDauRa);
+            Console.ReadLine();
+        }
+
+        static void NhiemVu1()
+        {
+            Console.WriteLine("Day la nhiem vu 1!");
+            Console.ReadLine();
+        }
+
+        static int NhiemVu2()
+        {
+           
+            int _KetQuaDauRa = 0;
+            _KetQuaDauRa = 4+5;
+            Console.WriteLine("Tra ra ket qua tong 4+5 = {0}", _KetQuaDauRa);
+            Console.ReadLine();
+            return _KetQuaDauRa;
+        }
+
+        static string NhiemVu3(int _BienKemTheo)
+        {
+            string _KQDR = "";
+            if (_BienKemTheo % 2 == 0)
+            {
+                _KQDR = "chan";
+            }
+            else
+            {
+                _KQDR = "le";
+            }
+            return _KQDR;
+        }
+
+        static string NhiemVu3(int _BienKemTheo1, int _BienKemTheo2)
+        {
+            int _KetQuaDauRa = 0;
+            _KetQuaDauRa =_BienKemTheo1+_BienKemTheo2;
+            Console.WriteLine("Tra ra ket qua tong 4+5 = {0}", _KetQuaDauRa);
+            Console.ReadLine();
+            return Convert.ToString(_KetQuaDauRa);
+        }
+    }
+}
